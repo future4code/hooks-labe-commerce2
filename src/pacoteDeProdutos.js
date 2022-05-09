c034d5b505f7694610fca2340122078a129f7209
CarlosToledoMartins <guto_highlander@hotmail.com>
2022-05-09 18:36:34 -0300

    home

---
+++ src/pacoteDeProdutos.js
@@ -0,0 +1,26 @@
+export const pacoteDeProdutos = [
+    {
+      id: 1,
+      name: 'Produto legal',
+      price: 120,
+      photo: 'https://picsum.photos/200/200?a=1',
+    },
+    {
+      id: 2,
+      name: 'Produto 2',
+      price: 200,
+      photo: 'https://picsum.photos/200/200?a=2'
+    },
+    {
+      id: 3,
+      name: 'Produto 3',
+      price: 180,
+      photo: 'https://picsum.photos/200/200?a=3'
+    },
+    {
+      id: 4,
+      name: 'Produto 4',
+      price: 100,
+      photo: 'https://picsum.photos/200/200?a=4'
+    }
+  ]
